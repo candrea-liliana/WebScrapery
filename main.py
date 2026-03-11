89db28c5cbbf1a6cc8191debbbcda65ee94486c2
Liliana Candrea <candrealiliana94@gmail.com>
2026-03-11 13:55:00 +0200

    Added scrapy summary for searches. Create a directory where to save the search results

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import os
 
 search = input("Enter search term: ")
 
@@ -12,17 +13,23 @@ driver.get(f"https://www.bing.com/search?q={search}")
 # Wait till the DOM is all loaded
 time.sleep(15)
 
-soup = BeautifulSoup(driver.page_source, "html.parser") # Organizing a structured tree out of HTML
-results = soup.find("ol", {"id": "b_results"}) #  find by tag and id
+soup = BeautifulSoup(driver.page_source, "html.parser") # Parsing a structured tree out of HTML
+results = soup.find("ol", {"id": "b_results"}) #  Find by tag and id
 
 driver.quit() # Closes all browser windows and fully shuts down the ChromeDriver process
+
+# Create the directory if it doesn't exist
+output_dir = "scraped_searches"
+os.makedirs(output_dir, exist_ok=True)
 
 if not results:
     print("No results found")
 else:
     links = results.find_all("li", {"class": "b_algo"})
 
-    with open(f"{search}_results.txt", "w", encoding="utf-8") as f:
+    filepath = os.path.join(output_dir, f"{search}_results.txt")
+
+    with open(filepath, "w", encoding="utf-8") as f:
         f.write(f"Search results for: {search}\n")
         f.write("=" * 50 + "\n\n")
 
@@ -41,12 +48,20 @@ else:
             if title == "No title" and url == "No URL":
                 continue
 
+            p_tag = item.find(class_="b_lineclamp2")
+            if p_tag:
+                summary = p_tag.text.strip() if p_tag else "No summary"
+            else:
+                summary = "No summary"
+
             print(title)
             print(url)
+            print("Summary:", summary)
             print("---")
 
             f.write(f"Title: {title}\n")
             f.write(f"URL:   {url}\n")
+            f.write(f"Summary:  {summary}\n")
             f.write("-" * 50 + "\n")
 
     print(f"\nResults saved to '{search}_results.txt'")
